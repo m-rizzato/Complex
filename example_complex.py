f1a2558b3026058169e0e5abc22b72d8a9d6eb3b
m-rizzato <76054041+m-rizzato@users.noreply.github.com>
2020-12-17 14:55:54 +0100

    Update example_complex.py

--- example_complex.py
+++ example_complex.py
@@ -1,28 +1,26 @@
-import Complex as C
+from Complex import MyComplex
 
-#creating two complex number 
-a = C.Complex( 2., 1.)
-b = C.Complex( 5., 0.)
-print('The complex number \'a\' has real and imaginary part (resp.): ' , a.R , a.I )
-print('The complex number \'b\' has real and imaginary part (resp.): ' , b.R , b.I )
+# creating two complex number
+a = MyComplex(2., 1.)
+b = MyComplex(5., 0.)
+print('The complex number \'a\' has real and imaginary part (resp.): ', a.Re, a.Im)
+print('The complex number \'b\' has real and imaginary part (resp.): ', b.Re, b.Im)
 
-#we can determine their complex sum c = a+b via the overloaded operator '+'
+# we can determine their complex sum c = a+b via the overloaded operator '+'
 c = a+b
-#we print the result and verify that the resulting variable 'c' is indeed an instance of the class 'Complex'
-print('\nTheir sum \'c\' has real and imaginary part (resp.): ' , c.R , c.I )
-print('The varialbe \'c\' has type: ', type(c))
+# we print the result and verify that the resulting variable 'c' is indeed an instance of the class 'Complex'
+print('\nTheir sum \'c\' has real and imaginary part (resp.): ', c.Re, c.Im)
+print('The variable \'c\' has type: ', type(c))
 
 
-#similarly, we can compute other basic complex operations between the complex number 'a' and 'b'
-c=a-b
-print('\nTheir difference has real and imaginary part (resp.): ' , c.R , c.I )
+# similarly, we can compute other basic complex operations between the complex number 'a' and 'b'
+c = a-b
+print('\nTheir difference has real and imaginary part (resp.): ', c.Re, c.Im)
 
-c=a*b
-print('\nTheir product has real and imaginary part (resp.): ' , c.R , c.I )
+c = a*b
+print('\nTheir product has real and imaginary part (resp.): ', c.Re, c.Im)
 
-c=a/b
-print('\nTheir ratio has real and imaginary part (resp.): ' , c.R , c.I )
+c = a/b
+print('\nTheir ratio has real and imaginary part (resp.): ', c.Re, c.Im)
 
-print('\nTheir respective modules are: ' , a.mod() , b.mod() )
-
-
+print('\nTheir respective modules are: ', a.mod(), b.mod())
