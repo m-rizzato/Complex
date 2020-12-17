e2cedecaea93e01033f23257b2d662b6a378e707
m-rizzato <76054041+m-rizzato@users.noreply.github.com>
2020-12-17 14:57:03 +0100

    Update test_Complex.py

--- test_Complex.py
+++ test_Complex.py
@@ -1,103 +1,109 @@
-import Complex as C
+from Complex import MyComplex
 from math import isclose 
 
-tollerance = 1e-6
+toll = 1e-6
 
-a1 = C.Complex( 2., 1.)
-b1 = C.Complex( 5., 0.) 	
+a1 = MyComplex(2., 1.)
+b1 = MyComplex(5., 0.)
 
-a2 = C.Complex(-2., 0.)
-b2 = C.Complex( 5.,-6.)
+a2 = MyComplex(-2., 0.)
+b2 = MyComplex(5., -6.)
 
-a3 = C.Complex( 0., 1.)
-b3 = C.Complex( 1., 0.)
+a3 = MyComplex(0., 1.)
+b3 = MyComplex(1., 0.)
 
-a4 = C.Complex( 5., 1.)
-b4 = C.Complex(-5.,-1.)
+a4 = MyComplex(5., 1.)
+b4 = MyComplex(-5., -1.)
 
-a5 = C.Complex( 2., 1.)
-b5 = C.Complex( 2., 1.)
+a5 = MyComplex(2., 1.)
+b5 = MyComplex(2., 1.)
 
-a6 = C.Complex( 10.,-10.)
-b6 = C.Complex( 15.,-15.)
+a6 = MyComplex(10., -10.)
+b6 = MyComplex(15., -15.)
 
-def compare_complex(re,im,true_re,true_im):
 
-    if not isclose(re, true_re, rel_tol=tollerance):
+def compare_complex(re, im, true_re, true_im):
+
+    if not isclose(re, true_re, rel_tol=toll):
         return False
 
-    if not isclose(im, true_im, rel_tol=tollerance):
+    if not isclose(im, true_im, rel_tol=toll):
         return False
 
     return True
 
-def test_sum( ):
+
+def test_sum():
     c1 = a1+b1
-    assert compare_complex(c1.R,c1.I, 7., 1.)
+    assert compare_complex(c1.Re, c1.Im, 7., 1.)
     c2 = a2+b2	
-    assert compare_complex(c2.R,c2.I, 3., -6.)
+    assert compare_complex(c2.Re, c2.Im, 3., -6.)
     c3 = a3+b3
-    assert compare_complex(c3.R,c3.I, 1., 1.)
+    assert compare_complex(c3.Re, c3.Im, 1., 1.)
     c4 = a4+b4
-    assert compare_complex(c4.R,c4.I, 0., 0.)
+    assert compare_complex(c4.Re, c4.Im, 0., 0.)
     c5 = a5+b5
-    assert compare_complex(c5.R,c5.I, 4., 2.)
+    assert compare_complex(c5.Re, c5.Im, 4., 2.)
     c6 = a6+b6
-    assert compare_complex(c6.R,c6.I, 25., -25.)
+    assert compare_complex(c6.Re, c6.Im, 25., -25.)
 
-def test_sub( ):
+
+def test_sub():
     c1 = a1-b1      
-    assert compare_complex(c1.R,c1.I,-3.,1.)
+    assert compare_complex(c1.Re, c1.Im, -3., 1.)
     c2 = a2-b2
-    assert compare_complex(c2.R,c2.I, -7., 6.)
+    assert compare_complex(c2.Re, c2.Im, -7., 6.)
     c3 = a3-b3
-    assert compare_complex(c3.R,c3.I, -1., 1.)
+    assert compare_complex(c3.Re, c3.Im, -1., 1.)
     c4 = a4-b4
-    assert compare_complex(c4.R,c4.I, 10., 2.)
+    assert compare_complex(c4.Re, c4.Im, 10., 2.)
     c5 = a5-b5
-    assert compare_complex(c5.R,c5.I, 0., 0.)
+    assert compare_complex(c5.Re, c5.Im, 0., 0.)
     c6 = a6-b6
-    assert compare_complex(c6.R,c6.I, -5., 5.)
+    assert compare_complex(c6.Re, c6.Im, -5., 5.)
 
-def test_prod( ):
+
+def test_prod():
     c1 = a1*b1      
-    assert compare_complex(c1.R,c1.I, 10., 5.)
+    assert compare_complex(c1.Re, c1.Im, 10., 5.)
     c2 = a2*b2
-    assert compare_complex(c2.R,c2.I, -10., 12.)
+    assert compare_complex(c2.Re, c2.Im, -10., 12.)
     c3 = a3*b3
-    assert compare_complex(c3.R,c3.I, 0., 1.)
+    assert compare_complex(c3.Re, c3.Im, 0., 1.)
     c4 = a4*b4
-    assert compare_complex(c4.R,c4.I, -24., -10.)
+    assert compare_complex(c4.Re, c4.Im, -24., -10.)
     c5 = a5*b5
-    assert compare_complex(c5.R,c5.I, 3., 4.)
+    assert compare_complex(c5.Re, c5.Im, 3., 4.)
     c6 = a6*b6
-    assert compare_complex(c6.R,c6.I, 0., -300.)
+    assert compare_complex(c6.Re, c6.Im, 0., -300.)
 
-def test_divid( ):
+
+def test_divid():
     c1 = a1/b1      
-    assert compare_complex(c1.R,c1.I, 0.4, 0.2)
+    assert compare_complex(c1.Re, c1.Im, 0.4, 0.2)
     c2 = a2/b2
-    assert compare_complex(c2.R,c2.I, -0.1639344262, -0.1967213114754098 )
+    assert compare_complex(c2.Re, c2.Im, -0.1639344262, -0.1967213114754098)
     c3 = a3/b3
-    assert compare_complex(c3.R,c3.I, 0., 1.)
+    assert compare_complex(c3.Re, c3.Im, 0., 1.)
     c4 = a4/b4
-    assert compare_complex(c4.R,c4.I, -1., 0.)
+    assert compare_complex(c4.Re, c4.Im, -1., 0.)
     c5 = a5/b5
-    assert compare_complex(c5.R,c5.I, 1., 0.)
+    assert compare_complex(c5.Re, c5.Im, 1., 0.)
     c6 = a6/b6
-    assert compare_complex(c6.R,c6.I, 0.6666666667, 0.)
+    assert compare_complex(c6.Re, c6.Im, 0.6666666667, 0.)
 
-def test_mod( ):
-    assert isclose(a1.mod(), 2.236067977, rel_tol=tollerance) 
-    assert isclose(a2.mod(), 2., rel_tol=tollerance)
-    assert isclose(a3.mod(), 1., rel_tol=tollerance)
-    assert isclose(a4.mod(), 5.099019514, rel_tol=tollerance)
-    assert isclose(a5.mod(), 2.236067977, rel_tol=tollerance)
-    assert isclose(a6.mod(), 14.14213562, rel_tol=tollerance)
 
-    assert isclose(b1.mod(), 5., rel_tol=tollerance)
-    assert isclose(b2.mod(), 7.810249676, rel_tol=tollerance)
-    assert isclose(b3.mod(), 1., rel_tol=tollerance)
-    assert isclose(b4.mod(), 5.09901951, rel_tol=tollerance)
-    assert isclose(b5.mod(), 2.236067977, rel_tol=tollerance)
-    assert isclose(b6.mod(), 21.21320344, rel_tol=tollerance)
+def test_mod():
+    assert isclose(a1.mod(), 2.236067977, rel_tol=toll)
+    assert isclose(a2.mod(), 2., rel_tol=toll)
+    assert isclose(a3.mod(), 1., rel_tol=toll)
+    assert isclose(a4.mod(), 5.099019514, rel_tol=toll)
+    assert isclose(a5.mod(), 2.236067977, rel_tol=toll)
+    assert isclose(a6.mod(), 14.14213562, rel_tol=toll)
+
+    assert isclose(b1.mod(), 5., rel_tol=toll)
+    assert isclose(b2.mod(), 7.810249676, rel_tol=toll)
+    assert isclose(b3.mod(), 1., rel_tol=toll)
+    assert isclose(b4.mod(), 5.09901951, rel_tol=toll)
+    assert isclose(b5.mod(), 2.236067977, rel_tol=toll)
+    assert isclose(b6.mod(), 21.21320344, rel_tol=toll)
